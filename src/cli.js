cd4b7c6e1e01c21e8ec32d18097a2f114383752e
Pavel M <striker746@mail.ru>
2022-08-22 10:15:17 +0300

    update game files

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+export const greeting = () => {
 console.log('May I have your name?');
-export default const name = readlineSync.question('Your answer: ');
-console.log('Hello, ' + name + '!');
+const name = readlineSync.question('Your answer: ');
+console.log(`Hello, ${name}!`);
+};
--- src/evenGame.js
+++ src/evenGame.js
@@ -1,37 +1,28 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-  console.log('May I have your name?')
+export const bgEven = () => {
+  console.log('May I have your name?');
   const name = readlineSync.question('Your answer: ');
-  console.log('Hello, ' + name + '!');
-}
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  const randomNumber = (maxNumb) => {
-    return Math.floor(Math.random() * maxNumb);
-  };
+  const randomNumber = (maxNumb) => Math.floor(Math.random() * maxNumb);
 
-    export const bg_even = () => {
-	console.log('May I have your name?')
-        const name = readlineSync.question('Your answer: ');
-        console.log('Hello, ' + name + '!');
+  for (let i = 0; i < 3; i += 1) {
+    const number = randomNumber(100);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
 
-	console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    const evenCheckTrue = (number % 2 === 0) && (answer === 'yes');
+    const evenCheckFalse = (number % 2 !== 0) && (answer === 'no');
 
-    for (let i = 0; i < 3; i += 1) {
-        const number = randomNumber(100);
-        console.log('Question: ' + number);
-        const answer = readlineSync.question('Your answer: ');
-
-        const evenCheckTrue = (number % 2 === 0) && (answer === 'yes');
-        const evenCheckFalse = (number % 2 !== 0) && (answer === 'no');
-
-        if (evenCheckTrue === true || evenCheckFalse === true) {
-            console.log('Correct!');
-        } else {
-            const anotherVar = (answer === 'yes' ? 'no' : 'yes');
-            const errorMessage = console.log(`"${answer}" is wrong answer ;(. Correct answer was '${anotherVar}'. \nLet's try again, ${name}!`);
-            return errorMessage;
-        };
-    };
-    console.log('Congratulations, ' + name + '!');
+    if (evenCheckTrue === true || evenCheckFalse === true) {
+      console.log('Correct!');
+    } else {
+      const anotherVar = (answer === 'yes' ? 'no' : 'yes');
+      const errorMessage = console.log(`"${answer}" is wrong answer ;(. Correct answer was '${anotherVar}'. \nLet's try again, ${name}!`);
+      return errorMessage;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 };
