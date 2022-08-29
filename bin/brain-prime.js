32c91343df846a0c4933abfa751b9ac5061a522c
Pavel M <striker746@mail.ru>
2022-08-29 12:30:04 +0300

    add brain-prime.js and primeGame.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import { bgPrime } from '../src/primeGame.js';
+bgPrime();
---
+++ src/primeGame.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+
+export const bgPrime = () => {
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  let divider = 2;
+  while (divider <= number / 2) {
+    if (number % divider === 0) {
+      return false;
+    }
+      divider += 1;
+    }
+      return true;
+    };
+
+  console.log('May I have your name?');
+  const name = readlineSync.question('Your answer: ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+  const randomNumber = (maxNumb) => Math.floor(Math.random() * maxNumb);
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = randomNumber(13);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    const primeCheckTrue = (isPrime(number) === true) && (answer === 'yes');
+    const primeCheckFalse = (isPrime(number) === false) && (answer === 'no');
+
+    if (primeCheckTrue === true || primeCheckFalse === true) {
+      console.log('Correct!');
+    } else {
+      const anotherVar = (answer === 'yes' ? 'no' : 'yes');
+      const errorMessage = console.log(`"${answer}" is wrong answer ;(. Correct answer was '${anotherVar}'. \nLet's try again, ${name}!`);
+      return errorMessage;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
